c87fc0901f581ed8c71a77f1a9c168e9a1fbf25e
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-10 18:59:42 +0300

    Scripted auto-commit on change (2020-10-10 18:59:42) by gitwatch.sh

--- src/main/java/laba2/TimeDelayCounter.java
+++ src/main/java/laba2/TimeDelayCounter.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
-public class WordCountApp {
+public class TimeDelayCounter {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.err.println("Usage: WordCountApp <input path> <output path>");
