51c4012b1d37eeb75edb24a25d8d9d1f57a94c26
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-10 18:55:33 +0300

    Scripted auto-commit on change (2020-10-10 18:55:33) by gitwatch.sh

--- src/main/java/laba2/CallsJoinMapper.java
+++ src/main/java/laba2/CallsJoinMapper.java
@@ -1,4 +1,10 @@
 package laba2;
 
-public class CallsJoinMapper {
+public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        ServiceCall call = new ServiceCall(value);
+        context.write(new TextPair(call.getSystemA().toString(),"1"),
+                new Text(call.toString()));
+    }
 }
