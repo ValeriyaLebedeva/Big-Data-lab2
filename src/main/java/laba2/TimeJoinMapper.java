a7f067c3e745c5efbff93552329f633ab136badf
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-11 16:41:20 +0300

    Scripted auto-commit on change (2020-10-11 16:41:20) by gitwatch.sh

--- src/main/java/laba2/TimeJoinMapper.java
+++ src/main/java/laba2/TimeJoinMapper.java
@@ -10,8 +10,8 @@ public class TimeJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] records = value.toString().split(",");
-        delay_time = records[18];
-        
+        String delay_time = records[18];
+        String airport_id = records[]
 
         context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString()));
     }
