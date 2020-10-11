8615d96e06561adabf465e2ff0cb1887ecf982ac
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-11 17:01:05 +0300

    Scripted auto-commit on change (2020-10-11 17:01:05) by gitwatch.sh

--- src/main/java/laba2/TextPair.java
+++ src/main/java/laba2/TextPair.java
@@ -1,13 +1,40 @@
 package laba2;
 
 
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+
+public class TextPair implements Writable {
+    public String first;
+    public String second;
+    public TextPair(){
+        this.first=first;
+        this.second=second;
+    }
+    public void write(DataOutput out) throws IOException {
+        out.writeChars(first);
+        out.writeChars(second);
+    }
+    public void readFields(DataInput in) throws IOException {
+        a = in.readInt();
+        b = in.readInt();
+    }
+    public String toString() {
+        return Integer.toString(a) + ", " + Integer.toString(b)
+    }
+}
+
 
 public class TextPair implements WritableComparable {
     public int first;
     public int second;
     public TextPair(){
-        this.first=second;
+        this.first=first;
         this.second=second;
     }
     public void write(DataOutput out) throws IOException {
