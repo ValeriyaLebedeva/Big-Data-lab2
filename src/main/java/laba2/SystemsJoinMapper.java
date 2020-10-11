14e18925594cae6a902ec683428c19e2ecfd353a
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-11 16:07:18 +0300

    Scripted auto-commit on change (2020-10-11 16:07:18) by gitwatch.sh

--- src/main/java/laba2/SystemsJoinMapper.java
+++ src/main/java/laba2/SystemsJoinMapper.java
@@ -1,8 +1,14 @@
 package laba2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         SystemInfo system = new SystemInfo(value);
         context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString()));
     }
--- src/main/java/laba2/TimeDelayCounter.java
+++ src/main/java/laba2/TimeDelayCounter.java
@@ -3,6 +3,8 @@ package laba2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
