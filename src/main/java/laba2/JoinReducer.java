5b49ab20022c967d9ae4289aa1eebac1b22a0cdf
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-11 16:12:18 +0300

    Scripted auto-commit on change (2020-10-11 16:12:18) by gitwatch.sh

--- src/main/java/laba2/JoinReducer.java
+++ src/main/java/laba2/JoinReducer.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
