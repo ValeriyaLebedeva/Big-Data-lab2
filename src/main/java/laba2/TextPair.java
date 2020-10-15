be5302cbbc14277e3ac69fb9d33e9d1c6fd9242a
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 13:02:50 +0300

    Scripted auto-commit on change (2020-10-15 13:02:50) by gitwatch.sh

--- src/main/java/laba2/TextPair.java
+++ src/main/java/laba2/TextPair.java
@@ -1,9 +1,5 @@
 package laba2;
 
-
-import com.google.common.primitives.Chars;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
@@ -25,6 +21,7 @@ public class TextPair implements WritableComparable {
         first = in.readLine();
         second = in.readLine();
     }
+    @Override
     public int compareTo(TextPair c){
         int presentValue=Integer.parseInt(this.first);
         int compareValue=Integer.parseInt(c.first);
@@ -34,10 +31,5 @@ public class TextPair implements WritableComparable {
         int a = Integer.parseInt(this.first);
         int b = Integer.parseInt(this.second);
         return a*10+b;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        return 0;
     }
 }
