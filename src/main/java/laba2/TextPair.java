c3b296c4d03e0c8bdda63b478dccdbbe2f8f24da
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 10:35:35 +0300

    Scripted auto-commit on change (2020-10-15 10:35:35) by gitwatch.sh

--- src/main/java/laba2/TextPair.java
+++ src/main/java/laba2/TextPair.java
@@ -9,74 +9,29 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-
-//public class TextPair implements Writable {
-//    public String first;
-//    public String second;
-//    public TextPair(){
-//        this.first=first;
-//        this.second=second;
-//    }
-//    public void write(DataOutput out) throws IOException {
-//        out.writeChars(first);
-//        out.writeChars(second);
-//    }
-//    public void readFields(DataInput in) throws IOException {
-//        first = in.readLine();
-//        second = in.readLine();
-//    }
-//    public String toString() {
-//        return first + ", " + second;
-//    }
-//}
-
-
-//public class TextPair implements WritableComparable {
-//    public String first;
-//    public String second;
-//    public TextPair(String first, String second){
-//        this.first=first;
-//        this.second=second;
-//    }
-//    public void write(DataOutput out) throws IOException {
-//        out.writeChars(first);
-//        out.writeChars(second);
-//    }
-//    public void readFields(DataInput in) throws IOException {
-//        first = in.readLine();
-//        second = in.readLine();
-//    }
-//    public int CompareTo(TextPair c){
-//        int presentValue=Integer.parseInt(this.first);
-//        int compareValue=Integer.parseInt(c.first);
-//        return (Integer.compare(presentValue, compareValue));
-//    }
-//    public int hashCode() {
-//        return Integer.IntToIntBits(a)^ Integer.IntToIntBits(b);
-//    }
-//}
-
-public class TextPair implements WritableComparable{
-    public int a;
-    public int b;
-    public TextPair(int a, int b){
-        this.a=a;
-        this.b=b;
+public class TextPair implements WritableComparable {
+    public String first;
+    public String second;
+    public TextPair(String first, String second){
+        this.first=first;
+        this.second=second;
     }
     public void write(DataOutput out) throws IOException {
-        out.writeint(a);
-        out.writeint(b);
+        out.writeChars(first);
+        out.writeChars(second);
     }
     public void readFields(DataInput in) throws IOException {
-        a = in.readint();
-        b = in.readint();
+        first = in.readLine();
+        second = in.readLine();
     }
-    public int CompareTo(TextPair c){
-        int presentValue=this.a;
-        int CompareValue=c.a;
-        return (Integer.compare(presentValue, CompareValue));
+    public int compareTo(TextPair c){
+        int presentValue=Integer.parseInt(this.first);
+        int compareValue=Integer.parseInt(c.first);
+        return (Integer.compare(presentValue, compareValue));
     }
     public int hashCode() {
-        return Integer.IntToIntBits(a)^ Integer.IntToIntBits(b);
+        int a = Integer.parseInt(this.first);
+        int b = Integer.parseInt(this.second);
+        return a*10+b;
     }
 }
