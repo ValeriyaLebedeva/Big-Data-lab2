3c3436e9e7c5656cb562c9e4570cc9bcd1154890
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 22:37:49 +0300

    Scripted auto-commit on change (2020-10-15 22:37:49) by gitwatch.sh

--- src/main/java/laba2/MyComparator.java
+++ src/main/java/laba2/MyComparator.java
@@ -3,6 +3,7 @@ package laba2;
 import org.apache.hadoop.io.RawComparator;
 
 public class MyComparator implements RawComparator {
+
     public int compare(TextPair a, TextPair b) {
         return a.compareTo(b);
     }
