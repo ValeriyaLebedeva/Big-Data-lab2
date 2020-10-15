828f55b74b9efb5e5ebdaef34194522ebe38071d
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 13:14:47 +0300

    Scripted auto-commit on change (2020-10-15 13:14:47) by gitwatch.sh

--- src/main/java/laba2/Partitioner.java
+++ src/main/java/laba2/Partitioner.java
@@ -1,4 +1,7 @@
 package laba2;
+package org.apache.hadoop.mapreduce;
 
-public class Partitioner {
+public abstract class Partitioner<KEY, VALUE> {
+    public abstract int getPartition(KEY key, VALUE value, int numPartitions);
+    
 }
