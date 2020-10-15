ae85c8a4f0927c8185bf0a02e949373517e2a544
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 22:24:45 +0300

    Scripted auto-commit on change (2020-10-15 22:24:45) by gitwatch.sh

--- src/main/java/laba2/TextPair.java
+++ src/main/java/laba2/TextPair.java
@@ -7,30 +7,30 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class TextPair implements WritableComparable {
-    public String first;
-    public String  second;
-    public TextPair(String first, String second){
-        this.first=first;
-        this.second=second;
+    public String idAirport;
+    public String  numTable;
+    public TextPair(String idAirport, String numTable){
+        this.idAirport =idAirport;
+        this.numTable=numTable;
     }
     public void write(DataOutput out) throws IOException {
-        out.writeChars(first);
-        out.writeChars(second);
+        out.writeChars(idAirport);
+        out.writeChars(numTable);
     }
     public void readFields(DataInput in) throws IOException {
-        first = in.readLine();
-        second = in.readLine();
+        idAirport = in.readLine();
+        numTable = in.readLine();
     }
 
     @Override
     public int compareTo(TextPair c){
-        int presentValue=Integer.parseInt(this.first);
-        int compareValue=Integer.parseInt(c.first);
+        int presentValue=Integer.parseInt(this.idAirport);
+        int compareValue=Integer.parseInt(c.idAirport);
         return (Integer.compare(presentValue, compareValue));
     }
     public int hashCode() {
-        int a = Integer.parseInt(this.first);
-        int b = Integer.parseInt(this.second);
+        int a = Integer.parseInt(this.idAirport);
+        int b = Integer.parseInt(this.numTable);
         return a*10+b;
     }
 }
