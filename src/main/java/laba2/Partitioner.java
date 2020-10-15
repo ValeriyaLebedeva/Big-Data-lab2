11a4b56ee9c347a2b1a279b32b61312e991e8bb8
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 22:01:26 +0300

    Scripted auto-commit on change (2020-10-15 22:01:26) by gitwatch.sh

--- src/main/java/laba2/Partitioner.java
+++ src/main/java/laba2/Partitioner.java
@@ -1,9 +1,13 @@
 package laba2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public abstract class MyPartitioner extends Partitioner<KEY, VALUE> {
-    public int getPartition(KEY key, VALUE value, int numPartitions) {
+
+
+public abstract class MyPartitioner extends Partitioner<TextPair, Text> {
+    @Override
+    public int getPartition(TextPair key, Text value, int numPartitions) {
         return (key.hashCode() & Integer.MAX_VALUE) % numPartitions;
     }
 }
