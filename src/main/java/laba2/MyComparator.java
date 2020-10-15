1442ca88d97181c6bc584bb8eb99365ff132702d
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 22:46:35 +0300

    Scripted auto-commit on change (2020-10-15 22:46:35) by gitwatch.sh

--- src/main/java/laba2/MyComparator.java
+++ src/main/java/laba2/MyComparator.java
@@ -1,15 +1,15 @@
 package laba2;
 
-import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.RawComparator;
 
-public class MyComparator implements WritableComparable<TextPair> {
+public class MyComparator implements RawComparator<TextPair> {
 
     public int compare(TextPair a, TextPair b) {
         return a.compareTo(b);
     }
 
-    @Override
-    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-        return 0;
-    }
+//    @Override
+//    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+//        return 0;
+//    }
 }
