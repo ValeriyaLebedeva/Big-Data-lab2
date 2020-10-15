3838544f7a2ecc5e3f9484993dbff709de6b747f
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-15 22:37:08 +0300

    Scripted auto-commit on change (2020-10-15 22:37:08) by gitwatch.sh

--- src/main/java/laba2/MyComparator.java
+++ src/main/java/laba2/MyComparator.java
@@ -11,9 +11,4 @@ public class MyComparator implements RawComparator {
     public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
         return 0;
     }
-
-    @Override
-    public int compare(Object o1, Object o2) {
-        return 0;
-    }
 }
