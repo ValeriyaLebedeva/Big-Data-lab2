609001f0012c02d736ebee46d0edf2be368fd9c1
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-16 20:04:27 +0300

    Scripted auto-commit on change (2020-10-16 20:04:27) by gitwatch.sh

--- src/main/java/laba2/idAirportPartitioner.java
+++ src/main/java/laba2/idAirportPartitioner.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class idAirportPartitioner extends Partitioner<WritableComparable, Text> {
+public abstract class idAirportPartitioner extends Partitioner<WritableComparable, Text> {
     @Override
     public int getPartition(WritableComparable key, Text value, int numPartitions) {
         KeyPair o = (KeyPair) key;
