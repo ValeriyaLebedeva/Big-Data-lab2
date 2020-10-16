7d88b8adb09676489b282626782609c2d3fb31ed
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-16 19:46:14 +0300

    Scripted auto-commit on change (2020-10-16 19:46:14) by gitwatch.sh

--- src/main/java/laba2/DescriptionMapper.java
+++ src/main/java/laba2/DescriptionMapper.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class DescriptionMapper extends Mapper<LongWritable, Text, KeyPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] records = value.toString().split(",");
+        String str = value.toString();
+        numSpiter
         if (records.length > 2) {
             context.write(new KeyPair(new Text(records[0]), new Text("0")), new Text(records[1] + "," + records[2]));
         } else if (records.length == 2){
