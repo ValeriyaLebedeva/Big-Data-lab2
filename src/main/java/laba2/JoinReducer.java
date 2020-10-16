549783a26ec885b918beab76620fdfe5906bd759
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-16 19:57:25 +0300

    Scripted auto-commit on change (2020-10-16 19:57:25) by gitwatch.sh

--- src/main/java/laba2/JoinReducer.java
+++ src/main/java/laba2/JoinReducer.java
@@ -11,7 +11,6 @@ public class JoinReducer extends Reducer<KeyPair, Text, Text, Text> {
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text AirportDescription = new Text(iter.next().toString());
-        System.out.println(AirportDescription.toString());
         float maxTime = Float.MIN_VALUE * -1;
         float minTime = Float.MAX_VALUE;
         float sumTime = 0;
