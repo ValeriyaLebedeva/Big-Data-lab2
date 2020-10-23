325c3c143853b4286c0f55f701d813ae680cdad6
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-23 21:48:36 +0300

    Scripted auto-commit on change (2020-10-23 21:48:36) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -4,5 +4,6 @@ public class AirportApp {
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
         JavaSparkContext sc = new JavaSparkContext(Hadoop conf);
+        JavaRDD<String> distFile = sc.textFile(Hadoop "war-and-peace-1.txt");
     }
 }
