849770b2e7e87cdc04f3d43e6a496bc4c49d7b99
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-23 21:46:00 +0300

    Scripted auto-commit on change (2020-10-23 21:46:00) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -2,6 +2,7 @@ package lab3;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
-        
+        SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
+        JavaSparkContext sc = new JavaSparkContext(Hadoop conf);
     }
 }
