dae5cd2fcc11c066ae193ccbf3b3258c7e6344ed
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-24 13:03:12 +0300

    Scripted auto-commit on change (2020-10-24 13:03:12) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -8,6 +8,9 @@ public class AirportApp {
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> lines = sc.textFile("/time_data.csv");
+        JavaRDD<String> lines_time = sc.textFile("/time_data.csv");
+        JavaRDD<String> lines_desc = sc.textFile("/desc_data.csv");
+        
+
     }
 }
