b92946da50e9cd35ccd601a2c523d4bb59cf2a48
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-24 12:47:58 +0300

    Scripted auto-commit on change (2020-10-24 12:47:58) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -1,11 +1,11 @@
 package lab3;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
-        SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
-        JavaSparkContext sc = new JavaSparkContext(Hadoop conf);
-        JavaRDD<String> distFile = sc.textFile(Hadoop "war-and-peace-1.txt");
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
