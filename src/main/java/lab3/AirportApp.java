e2bd3e3b7c3c53a3788025a464643da77142ae0c
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-31 11:19:48 +0300

    Scripted auto-commit on change (2020-10-31 11:19:48) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -29,10 +29,11 @@ public class AirportApp {
                 s -> new Tuple2<>(removeQuotes(s[ORIGIN_AIRPORT_ID])+ ";" +removeQuotes(s[DEST_AIRPORT_ID]), s[NUM_DELAY_TIME])
         );
 
-        
+
         JavaPairRDD<String, String> timeDelayMax = timeDelayFlight.groupByKey().mapValues(AirportApp::getMaxTime);
-        JavaPairRDD<String, String> TmeDelayMaxOut = timeDelayMax.map(s -> s._1.split(";")[0]
-        )
+        JavaPairRDD<String, String> TmeDelayMaxOut = timeDelayMax.map(s ->
+                new Tuple2<>(glossary.lookup(s._1.split(";")[0])[0], s._2)
+        );
 
 
         timeDelayMaxOut.saveAsTextFile("output");
