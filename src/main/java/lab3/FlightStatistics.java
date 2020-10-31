63e288d514aeea765054a20cd626b26df96e5f41
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-10-31 16:35:33 +0300

    Scripted auto-commit on change (2020-10-31 16:35:33) by gitwatch.sh

--- src/main/java/lab3/FlightStatistics.java
+++ src/main/java/lab3/FlightStatistics.java
@@ -4,11 +4,31 @@ public class FlightStatistics {
     String percentCancelled;
     String percentDelay;
     String timeDelay;
+    String cancelled;
+    String delay;
 
-    public FlightStatistics(String percentCancelled, String percentDelay, String timeDelay) {
-        this.percentCancelled = percentCancelled;
-        this.percentDelay = percentDelay;
-        this.timeDelay = timeDelay;
+    public String getCancelled() {
+        return cancelled;
     }
 
+    public String getDelay() {
+        return delay;
+    }
+
+    public FlightStatistics(String delay, String cancelled) {
+        this.cancelled = cancelled;
+        this.delay = delay;
+    }
+
+    public String getPercentCancelled() {
+        return percentCancelled;
+    }
+
+    public String getPercentDelay() {
+        return percentDelay;
+    }
+
+    public String getTimeDelay() {
+        return timeDelay;
+    }
 }
